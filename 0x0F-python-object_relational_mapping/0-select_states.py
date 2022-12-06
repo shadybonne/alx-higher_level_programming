8b3719a0857a948c426d997efffb2c3021b690d3
shadybonne <shadybonne@gmail.com>
2022-12-06 03:48:20 -0800

    sql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# This script Lists all states from the database hbtn_0e_0_usa.
+"""
+ This script Lists all states from the database hbtn_0e_0_usa.
+"""
 import MySQLdb
 import sys
 if __name__ == "__main__":
