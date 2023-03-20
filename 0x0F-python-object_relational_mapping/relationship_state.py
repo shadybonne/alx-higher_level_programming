a8d5398963c579377a9e6d71d645f60bf7314b45
shadybonne <shadybonne@gmail.com>
2023-03-20 11:12:37 -0700

    mapping

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,19 +3,21 @@
 State class and an instance of declarative_base() representation
 """
 import sys
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
-
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+eat
 
 class State(Base):
-        """
+    """
     Representation of State class
         args:   __tablename___(str): Name of the table
                 id (int): unique identifier of table's row
                 name (str): name of states
     """
     __tablename__ = 'states'
-    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='states')
